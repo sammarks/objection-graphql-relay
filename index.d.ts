0a00be570b9169ff34d5da64d328e59a8e3a6689
George Pickering <29524044+geopic@users.noreply.github.com>
2020-02-28 13:43:19 +0000

    fix: add type declaration file

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,32 @@
-import { Model, ModelClass } from 'objection';
+import { Model, ModelClass, QueryBuilder } from 'objection';
 
 declare module 'objection-graphql-relay' {
-  export function idWrapper(modelName?: string): (parent: Model) => Promise<string>;
-  export function fromGlobalId(modelName?: string, globalId?: string): string;
-  export function singleRelationshipWrapper(relationshipName: string): Promise<string>;
-  export function connectionWrapper(field: string | object): Promise<object>;
-  export function pagedRelationQuery(instance: ModelClass<Model>, field: string | object, first: number, after: number, extraFilter?: (builder: any) => object, orderBy?: (builder: any) => object): Promise<object>;
-  export function range(first?: number, after?: number, deferred?: object): Promise<any>;
-  export function relayModel(ModelClass: ModelClass<Model>): ModelClass<Model>;
+  interface ConnectionWrapperResults {
+    results: any[];
+    total: number;
+  }
+
+  type ConnectionWrapperField = string | any[] | ConnectionWrapperResults;
+
+  interface Edge {
+    cursor: string;
+    node: Model;
+  }
+
+  interface Connection {
+    pageInfo: {
+      hasPreviousPage: boolean;
+      hasNextPage: boolean;
+    }
+    edges: Edge[];
+    totalCount: number;
+  }
+
+  export function idWrapper(modelName: string): (parent: Model) => Promise<string>;
+  export function fromGlobalId(modelName: string, globalId: string): string;
+  export function singleRelationshipWrapper(relationshipName: string): (parent: Model) => Promise<ModelClass<Model>> | Promise<null>;
+  export function connectionWrapper(field: ConnectionWrapperField): Connection;
+  export function pagedRelationQuery(instance: ModelClass<Model>, field: string | object, first: number, after: number, extraFilter?: (builder: QueryBuilder<Model>) => object, orderBy?: (builder: QueryBuilder<Model>) => object): Promise<ConnectionWrapperResults>;
+  export function range(first: number, after: number, deferred: Promise<QueryBuilder<Model>>): (builder: QueryBuilder<Model>) => void;
+  export function relayModel<T extends ModelClass<Model>>(ModelClass: T): T;
 }
